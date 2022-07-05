3ae5394ac2047765ce1feb22f0447a1b6bfa5497
bryan <voidpointers@hotmail.com>
2022-07-05 21:20:00 +0800

    use es6 set unique

--- views/supplier/index.php
+++ views/supplier/index.php
@@ -85,18 +85,16 @@ $this->registerJs(
                 ids.push($(this).val());
             });
 
-            $('.export-submit').click(function() {
-                $.fileDownload("$url", {
-                    data: {
-                        'id': ids,
-                        'columns': columns,
-                        'is_all': $('input[name="is-select-all"]').val()
-                    },
-                    dataType: 'json',
-                    success: function (result) {
-                        console.log(result);
-                    }
-                });
+            $.fileDownload("$url", {
+                data: {
+                    'id': ids,
+                    'columns': columns,
+                    'is_all': $('input[name="is-select-all"]').val()
+                },
+                dataType: 'json',
+                success: function (result) {
+                    console.log(result);
+                }
             });
             $('.close').click();
         });
@@ -126,6 +124,10 @@ $this->registerJs(
             }
         }
 
+        $('input[name="selection[]"]').on('click', function () {
+            var val = $(this).val();
+            checkbox(val, $(this).prop('checked'));
+        });
         $('input[name="selection_all"]').on('click', function () {
             if ($(this).prop('checked')) {
                 $('.select-tips-page').addClass('d-block');
@@ -141,7 +143,6 @@ $this->registerJs(
             for(var i = 0; i < items.length; i++) {
                 $('tr[data-key=' + items[i] + '] > td > input').prop('checked', true);
             }
-            // $('.select-tips-page').addClass('d-block');
         }
 
         if (sessionStorage.getItem('select-all')) {
@@ -173,35 +174,53 @@ $this->registerJs(
             }
         }
 
-        function checkPage(type = true, page)
+        function checkbox(val, type = true)
         {
             var ids = JSON.parse(sessionStorage.getItem('select-checked'));
             if (!ids) ids = [];
-            $('input[name="selection[]"]').each(function(i) { 
-                console.log($(this).val());
-                if (type) {
-                    ids.push($(this).val());
-                } else {
-                    var index = $.inArray($(this).val(), ids);
-                    ids.splice(index, 1);
-                }
-            });
-            console.log(ids);
+            if (type) {
+                ids.push(val);
+            } else {
+                var index = $.inArray(val, ids);
+                ids.splice(index, 1);
+            }
 
             sessionStorage.setItem(
                 'select-checked', JSON.stringify(ids)
             );
+        }
+
+        function checkPage(type = true, page)
+        {
+            var ids = JSON.parse(sessionStorage.getItem('select-checked'));
+            // Duplicate removal
+            ids = !ids ? new Set() : new Set(ids);
+
+            $('input[name="selection[]"]').each(function(i) { 
+                if (type) {
+                    // ids.push($(this).val());
+                    ids.add($(this).val());
+                } else {
+                    // var index = $.inArray($(this).val(), ids);
+                    // ids.splice(index, 1);
+                    ids.delete($(this).val())
+                }
+            });
+
+            sessionStorage.setItem(
+                'select-checked', JSON.stringify([...ids])
+            );
 
             var select = JSON.parse(sessionStorage.getItem('select-page'));
-            if (!select) select = [];
+            select = !select ? new Set() : new Set(select);
+
             if (type) {
-                select.push(page);
+                select.add(page);
             } else {
-                var index = $.inArray(page, select);
-                select.splice(index, 1);
+                select.delete(page);
             }
             sessionStorage.setItem(
-                'select-page', JSON.stringify(select)
+                'select-page', JSON.stringify([...select])
             );
         }
 JS
